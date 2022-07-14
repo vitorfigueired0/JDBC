f27932f472c551e4589529f761e47f0e64106bf1
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2022-07-14 09:28:14 -0300

    JDBC.V2 Inserindo dados

--- JavaProject/src/application/Main.java
+++ JavaProject/src/application/Main.java
@@ -2,37 +2,48 @@ package application;
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
 
         try{
             connection = DB.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM Department");
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO Seller"
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentID)"
+                    + "Values"
+                    + "(?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, "anna");
+            preparedStatement.setString(2, "anna@gmail.com");
+            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("02/01/2002").getTime()));
+            preparedStatement.setDouble(4, 1750);
+            preparedStatement.setInt(5, 3);
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt("ID")
-                        + ", "
-                        + resultSet.getString("Name"));
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0){
+                ResultSet set = preparedStatement.getGeneratedKeys();
+                while (set.next()){
+                    int id = set.getInt(1);
+                    System.out.println("DONE, ID = " + id);
+                }
+            }
+            else {
+                System.out.println("NO ROWS AFFECTED");
             }
         }
         catch (SQLException e){
             throw new DbException(e.getMessage());
         }
-        finally {
-            DB.closeStatement(statement);
-            DB.closeResulSet(resultSet);
-            DB.closeConnection();
+        catch (ParseException e){
+            throw new DbException(e.getMessage());
         }
-
     }
 }
